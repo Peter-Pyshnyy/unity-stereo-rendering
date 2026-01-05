666873e9e30b33586f080eaae112ceabfa42c152
Peter-Pyshnyy <62466617+Peter-Pyshnyy@users.noreply.github.com>
2026-01-05 22:38:25 +0530

    Implement ITD (bug)

--- Assets/Tutorial 5/Scripts/AudioController.cs
+++ Assets/Tutorial 5/Scripts/AudioController.cs
@@ -89,21 +89,60 @@ namespace Tutorial_5
         private void ApplyILD(float[] data, int channels)
         {
             float theta = angle * (float)Math.PI / 180f;
-            float tmp = Mathf.Sin(theta);
+            float intensity = Mathf.Sin(theta);
 
             // channgels always 2 (stereo); data interleaved LRLRLR...
             for (int i = 0; i < data.Length; i += 2)
             {
                 // Left channel
-                data[i] *= Mathf.Clamp01(0.5f - tmp * 0.5f);
+                data[i] *= Mathf.Clamp01(0.5f - intensity * 0.5f);
                 // Right channel
-                data[i + 1] *= Mathf.Clamp01(0.5f + tmp * 0.5f);
+                data[i + 1] *= Mathf.Clamp01(0.5f + intensity * 0.5f);
             }
         }
 
      
         private void ApplyITD(float[] data, int channels)
         {
+            float r = 0.1f; // head radius in meters
+            float C = 343f; // speed of sound in m/s
+            float theta = angle * (float)Math.PI / 180f;
+            float delay = r * (theta + Mathf.Sin(theta)) / C;
+            float delayInSamples = delay * sampleRate;
+            int shift = (int)Mathf.Abs(delayInSamples);
+
+            // left and right delay samples
+            if (delayInSamples < 0) { 
+                for (int i = 0; i < shift; i++)
+                {
+                    leftDelayBuffer[i] = 0.0f;
+                }
+                for (int i = 0; i < bufferSize - shift; i++)
+                {
+                    leftDelayBuffer[i + shift] = data[i * 2];
+                }
+            } else
+            {
+                for (int i = 0; i < shift; i++)
+                {
+                    rightDelayBuffer[i] = 0.0f;
+                }
+                for (int i = 0; i < bufferSize - shift; i++)
+                {
+                    rightDelayBuffer[i + shift] = data[i * 2 + 1];
+                }
+            }
+
+            for (int i = 0; i < data.Length; i += 2)
+            {
+                // Left channel
+                data[i] = leftDelayBuffer[leftReadIndex];
+                leftReadIndex = (leftReadIndex + 1) % bufferSize;
+                // Right channel
+                data[i + 1] = rightDelayBuffer[rightReadIndex];
+                rightReadIndex = (rightReadIndex + 1) % bufferSize;
+            }
+
         }
 
         private void UpdateParamsFromScene()
